31affaff5e556ec95954be04f993f269a8f3703f
Linda Paz <lindacsndelapaz@gmail.com>
2023-02-01 12:02:09 -0300

    add person variable

--- src/app/interfaces/persona.ts
+++ src/app/interfaces/persona.ts
@@ -1,3 +1,11 @@
 export interface Persona {
-  id: number;
+  id?: number;
+  nombre: string;
+  apellido: string;
+  correo: string;
+  tipoDocumento: string;
+  documento: number;
+  fechaNacimiento: Date;
 }
+
+// Los campos opcionales en tyscrip llevan signo de pregunta
